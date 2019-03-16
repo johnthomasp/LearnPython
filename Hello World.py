ffcbbdeb6a5b6d5063bcff8f2baab12f02b02e81
John Thomas <johnthomasp@gmail.com>
2019-03-15 18:47:40 -0700

    First commit

---
+++ Hello World.py
@@ -0,0 +1,17 @@
+# print("Hello World")
+# print("John Thomas")
+#price = 10
+#print (price)
+
+#name = "John S"
+#age = 20
+#is_newpatient = False
+
+name = input("What is your name? ")
+print("Hi " + name)
+favcolor = input("What is your fave color? ")
+print(name + " likes " + favcolor)
+
+
+
+#print("Name " + name + "Age " + str(age) + " is new patient " + str(is_newpatient))
